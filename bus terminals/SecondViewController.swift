6c4954220e7f0b8304d9eabecd19ffb7cd3b71fd
Nik <apple@macbook-pro-apple-3.local>
2016-04-14 11:42:39 +0400

    Initial Commit

---
+++ bus terminals/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  bus terminals
+//
+//  Created by Nik on 14.04.16.
+//  Copyright © 2016 Gafurov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
